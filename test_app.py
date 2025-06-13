dac49ffff02b874930c715a25c3e99c40a673d39
Krystian <krystian.ostrogorski@gmail.com>
2025-06-13 08:49:10 +0200

    Add HTTP endpoint test with pytest and fixture

--- test_app.py
+++ test_app.py
@@ -1,4 +1,6 @@
-from app import add
+from app import add,app
+import pytest
+
 
 def test_add_positive_numbers():
     assert add(2,3) == 5
@@ -8,3 +10,15 @@ def test_add_zero():
 
 def test_add_negative_numbers():
     assert add(-1,-2) == -3
+
+    
+@pytest.fixture
+def client():
+    app.testing = True
+    with app.test_client() as client:
+        yield client
+
+def test_root_endpoint(client):
+    response = client.get('/janusz')
+    assert response.status_code == 200
+    assert b"Hello, Janusz!" in response.data
